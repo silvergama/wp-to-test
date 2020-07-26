950ef7dd8cb0f0de1c1ce86d33d22e111e528d2d
Silver Gama <silver.mdg@gmail.com>
2020-07-25 22:17:34 -0300

    Remove more unused plugins

--- app/wp-content/plugins/woocommerce-payment-discounts/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// If uninstall not called from WordPress, then exit
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
-}
-
-// Delete plugin options.
-delete_option( 'woocommerce_payment_discounts' );
-delete_option( 'woocommerce_payment_discounts_version' );
